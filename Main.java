d269a4b899ad1d0434d5eaed29353556e15fcc3e
Kyanni <kyanni.gonzalez2027@gbwl.org>
2026-01-29 19:26:25 +0000

    Started Arryas HORRAYY!!

--- Main.java
+++ Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+     
+      // 1D ARRAY is a block of memory that stores a COLLECTION 
+      //of data items (elements)
+      //UNDER ONE VARIABLE NAME!!
+
+      //How to DECLARE an ARRAY VARIABLE 
+      String[] apCompsci = new String[13]; 
+      double[] prices = new double[5]; //default values: 0.0
+      String[] names = new String[5]; //defult values: null
+      int[] uckyNums= new int[10]; //default values: 0
+      boolean[] yesNo = new boolean[3]; // default values: false 
+
+      //How to ACCESS an item in an Array
+      System.out.println( prices[0]);
+
+
+
+
+
+
+
+
+   }//ends the main method
+} //ends the Main CLASS

96ceb7d715802f5576343ddb3b4c9d254040917f
Kyanni <kyanni.gonzalez2027@gbwl.org>
2026-02-06 13:54:43 +0000

    matthew helped fill in he notes

--- Main.java
+++ Main.java
@@ -16,6 +16,30 @@ public class Main {
       //How to ACCESS an item in an Array
       System.out.println( prices[0]);
 
+      //Use the same syntax to SET a value in an Array 
+      luckyNums [0] = 13;
+      luckyNums [5] = 7; 
+      System.out.println(luckyNums[0]);
+      System.out.println(luckyNums [5]);
+      System.out.println(luckyNums[3]); 
+
+      //Create an array win values using an INITIALIZER LIST
+      String[] table = {"Matthew", "Danny", "Kyanni"};
+      System.out.println(table[1]);
+      System.out.println(table [2]); 
+
+      //The .length ATTRIBUTE gets the NUMBER OF ITEMS in array 
+      int lengthOfTableArray = table.length; 
+      System.out.println(lengthOfTableArray);
+      //To get the LAST INDEX use: [arrayName.length -1];
+      int finalIndex = table.length -1; 
+      System.out.println(finalIndex); //2 is the final index 
+      System.out.println(table [finalIndex]); 
+      System.out.println(table [table.length -1]); 
+
+      double[] scores = {100.0, 100.0, 100.0, 91.54};
+      //look at one item using index 
+      System.out.println(scores[1]); 
 
 
 
